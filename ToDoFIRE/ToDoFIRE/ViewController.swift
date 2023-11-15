d2132b6ad4f6d64214a75373b05f03db6f97768c
mangraa1 <georgiu.spare@gmail.com>
2023-11-15 21:47:50 +0200

    main screen customization

---
+++ ToDoFIRE/ToDoFIRE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoFIRE
+//
+//  Created by mac on 15.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
